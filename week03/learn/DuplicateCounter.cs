f1f1c3cf94d4abee85f54da882a0cc7eadeff9ef
j-kleine <joschuadkleine@gmail.com>
2024-10-03 18:10:55 +0200

    w03 learning activity sets finished

--- week03/learn/DuplicateCounter.cs
+++ week03/learn/DuplicateCounter.cs
@@ -25,6 +25,21 @@
     private static int CountDuplicates(int[] data)
     {
         // Add code here.
-        return 0;
+        var uniques = new HashSet<int>();
+        var duplicates = 0;
+
+        foreach (var x in data)
+        {
+            if (uniques.Contains(x))
+            {
+                duplicates++;
+            }
+            else
+            {
+                uniques.Add(x);
+            }
+        }
+
+        return duplicates;
     }
 }
